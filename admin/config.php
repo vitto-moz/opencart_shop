0ded6af7f89effb2face492d6206c8ecb0235607
vitto-moz <v.mozharovsky@gmail.com>
2016-09-10 11:34:26 +0300

    First commit for downloading in remote repo

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://installopencart.localhost/admin/');
+define('HTTP_CATALOG', 'http://installopencart.localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://installopencart.localhost/admin/');
+define('HTTPS_CATALOG', 'http://installopencart.localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'Y:/OpenServer/domains/installopencart.localhost/admin/');
+define('DIR_SYSTEM', 'Y:/OpenServer/domains/installopencart.localhost/system/');
+define('DIR_IMAGE', 'Y:/OpenServer/domains/installopencart.localhost/image/');
+define('DIR_LANGUAGE', 'Y:/OpenServer/domains/installopencart.localhost/admin/language/');
+define('DIR_TEMPLATE', 'Y:/OpenServer/domains/installopencart.localhost/admin/view/template/');
+define('DIR_CONFIG', 'Y:/OpenServer/domains/installopencart.localhost/system/config/');
+define('DIR_CACHE', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/download/');
+define('DIR_LOGS', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/logs/');
+define('DIR_MODIFICATION', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/modification/');
+define('DIR_UPLOAD', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/upload/');
+define('DIR_CATALOG', 'Y:/OpenServer/domains/installopencart.localhost/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'interplace');
+define('DB_PASSWORD', '5955987592q!');
+define('DB_DATABASE', 'interplace');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://installopencart.localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://installopencart.localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'Y:/OpenServer/domains/installopencart.localhost/catalog/');
+define('DIR_SYSTEM', 'Y:/OpenServer/domains/installopencart.localhost/system/');
+define('DIR_IMAGE', 'Y:/OpenServer/domains/installopencart.localhost/image/');
+define('DIR_LANGUAGE', 'Y:/OpenServer/domains/installopencart.localhost/catalog/language/');
+define('DIR_TEMPLATE', 'Y:/OpenServer/domains/installopencart.localhost/catalog/view/theme/');
+define('DIR_CONFIG', 'Y:/OpenServer/domains/installopencart.localhost/system/config/');
+define('DIR_CACHE', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/download/');
+define('DIR_LOGS', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/logs/');
+define('DIR_MODIFICATION', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/modification/');
+define('DIR_UPLOAD', 'Y:/OpenServer/domains/installopencart.localhost/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'interplace');
+define('DB_PASSWORD', '5955987592q!');
+define('DB_DATABASE', 'interplace');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
